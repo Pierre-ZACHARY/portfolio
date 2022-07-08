6d2f494a0324096215eed0dcb1ca21513896a651
Pierre-ZACHARY <87607183+Pierre-ZACHARY@users.noreply.github.com>
2022-07-08 16:12:09 +0200

    08/07

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import {useTranslation} from "next-i18next";
 
 export default function Index({ allPostsData}: any) {
-    //const { t } = useTranslation('footer');
 
     return (
         <>
@@ -18,7 +17,7 @@ export async function getStaticProps({ locale }: any) {
     const allPostsData = getSortedPostsData();
     return {
         props: {
-            ...(await serverSideTranslations(locale, ['common'])),
+            ...(await serverSideTranslations(locale, ['common', 'header'])),
             allPostsData,
         },
     };
