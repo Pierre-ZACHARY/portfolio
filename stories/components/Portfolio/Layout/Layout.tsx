b223d855dc2cf4dbaa507adb95a131e96857a85f
Zachary Pierre <pierre.zachary@etu.univ-orleans.fr>
2022-08-30 01:25:43 +0200

    disabled auto-zoom

--- stories/components/Portfolio/Layout/Layout.tsx
+++ stories/components/Portfolio/Layout/Layout.tsx
@@ -1,10 +1,15 @@
 import {Header} from "./Header/header";
 import {Chatbot} from "./Chatbot/chatbot";
+import Head from "next/head";
 
 
 export const Layout = (props: any) => {
     return (
         <>
+            <Head>
+                <title>Pierre Zachary</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+            </Head>
             <div className={props.className}>
                 <main style={{paddingTop: props.paddingTop ?? "2em" }}>
                     {props.children}
